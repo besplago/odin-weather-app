7e6d82126e4afe1127bff310e3e124eb7d645381
besplago <bruce_esplago@hotmail.com>
2025-09-12 21:40:02 +0200

    Refactor getPlayers function to load players from a local file instead of API

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
 import "./styles.css";
 import "./template.html";
+
+async function getPlayers() {
+  // TODO: Instead of using API, just load players from a local file instead.
+  //   const response = await fetch("https://api.balldontlie.io/v1/players", {
+  //     headers: { Authorization: "ab7d3912-b6a7-42f1-acec-11659c260395" },
+  //   });
+  //   return response.json();
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  getPlayers().then((response) => console.log(response));
+});
