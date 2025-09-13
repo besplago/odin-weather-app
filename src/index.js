b989e8d2a3496fe84ddf2c20f3e796fe722fc909
besplago <bruce_esplago@hotmail.com>
2025-09-13 18:16:01 +0200

    feat: embedded youtube video

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import "./styles.css";
 import "./template.html";
 
-async function getPlayers() {
+const key = "ELSDJPML3MP99HR775X4UTM2N";
+
+async function fetchWeather() {
   // TODO: Instead of using API, just load players from a local file instead.
-  //   const response = await fetch("https://api.balldontlie.io/v1/players", {
-  //     headers: { Authorization: "ab7d3912-b6a7-42f1-acec-11659c260395" },
-  //   });
-  //   return response.json();
+  const today = new Date();
+  const dd = String(today.getDate()).padStart(2, "0");
+  const mm = String(today.getMonth() + 1).padStart(2, "0");
+  const yyyy = today.getFullYear();
+  const response = await fetch(
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/copenhagen/${yyyy}-${mm}-${dd}/?key=${key}`
+  );
+  return response.json();
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  getPlayers().then((response) => console.log(response));
+  // fetchWeather().then((response) => console.log(response));
 });
