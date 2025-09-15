f5088bc291a31378e1934daa875f6634bedac45f
besplago <bruce_esplago@hotmail.com>
2025-09-15 21:23:57 +0200

    refactor: simplify Weather constructor and remove unused initWeather method

--- src/js/weatherModel.js
+++ src/js/weatherModel.js
@@ -1,11 +1,20 @@
 export class Weather {
-  constructor(temperature, city, country, weatherDescription, windSpeed) {
-    // this.fetchWeatherResponse().then((weatherData) =>
-    //   this.initWeather(weatherData)
-    // );
+  constructor() {
+    this.temperature = null;
+    this.city = null;
+    this.country = null;
+    this.weatherDescription = null;
+    this.windSpeed = null;
+
+    this.loaded = false;
   }
 
-  initWeather(weatherData) {
-    console.log(weatherData);
+  setData(temperature, city, country, weatherDescription, windSpeed) {
+    this.temperature = temperature;
+    this.city = city;
+    this.country = country;
+    this.weatherDescription = weatherDescription;
+    this.windSpeed = windSpeed;
+    this.loaded = true;
   }
 }
