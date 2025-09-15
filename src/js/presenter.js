764d28f3de0c20011d2004bb96663837f8bea323
besplago <bruce_esplago@hotmail.com>
2025-09-15 21:18:57 +0200

    refactor: move fetching of weather data to presenter

--- src/js/presenter.js
+++ src/js/presenter.js
@@ -10,4 +10,12 @@ export class Presenter {
   updateTime = () => {
     this.view.setTime(this.time.getTime());
   };
+
+  async fetchWeatherResponse() {
+    const key = "852f08d906934fd18d9191846251109";
+    const response = await fetch(
+      `https://api.weatherapi.com/v1/current.json?key=${key}&q=London&aqi=no`
+    );
+    return response.json();
+  }
 }
--- src/js/weatherModel.js
+++ src/js/weatherModel.js
@@ -1,19 +1,11 @@
 export class Weather {
-  constructor() {
-    this.fetchWeatherResponse().then((weatherData) =>
-      this.initWeather(weatherData)
-    );
+  constructor(temperature, city, country, weatherDescription, windSpeed) {
+    // this.fetchWeatherResponse().then((weatherData) =>
+    //   this.initWeather(weatherData)
+    // );
   }
 
   initWeather(weatherData) {
     console.log(weatherData);
-  }
-
-  async fetchWeatherResponse() {
-    const key = "852f08d906934fd18d9191846251109";
-    const response = await fetch(
-      `https://api.weatherapi.com/v1/current.json?key=${key}&q=London&aqi=no`
-    );
-    return response.json();
   }
 }
