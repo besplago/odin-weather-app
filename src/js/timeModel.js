e463e33c640aeeb9f181fd633e3f475a71d91155
besplago <bruce_esplago@hotmail.com>
2025-09-17 21:14:29 +0200

    feat: 24hr time

--- src/js/timeModel.js
+++ src/js/timeModel.js
@@ -4,11 +4,19 @@ export class Time {
   }
 
   tick() {
-    this.currentTime.setSeconds(this.currentTime.getSeconds() + 1);
+    if (this.currentTime) {
+      this.currentTime.setSeconds(this.currentTime.getSeconds() + 1);
+    }
   }
 
   getTime() {
-    return this.currentTime.toLocaleTimeString();
+    if (!this.currentTime) return "";
+    return this.currentTime.toLocaleTimeString([], {
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+      hour12: false,
+    });
   }
 
   setStartTime(startTimeString) {
